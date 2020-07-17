c8ed2ad05e7e043acdae47a42dd54991f4700e60
破羽残蝶 <artoria1771@outlook.com>
2020-07-17 22:13:46 +0800

    Site updated: 2020-07-17 22:13:44

--- js/assets/waifu-tips.js
+++ js/assets/waifu-tips.js
@@ -66,8 +66,8 @@ live2d_settings['waifuDraggableRevert'] = false;         // 松开鼠标还原�
 // 其他杂项设置
 live2d_settings['l2dVersion']           = '1.4.2';        // 当前版本
 live2d_settings['l2dVerDate']           = '2018.11.12'; // 版本更新日期
-live2d_settings['homePageUrl']          = 'auto';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
-live2d_settings['aboutPageUrl']         = 'https://www.fghrsh.net/post/123.html';   // 关于页地址, '{URL 网址}'
+live2d_settings['homePageUrl']          = 'https://twitter.com/eDziXu191xjiLP5';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
+live2d_settings['aboutPageUrl']         = 'https://bbs.yamibo.com/forum-30-1.html';   // 关于页地址, '{URL 网址}'
 live2d_settings['screenshotCaptureName']= 'live2d.png'; // 看板娘截图文件名，例如 'live2d.png'
 
 /****************************************************************************************************/
